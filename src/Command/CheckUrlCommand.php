f6a91b013bf5ae99b76adc503bf8fe913e71729d
mariusz <mtekiel777@gmail.com>
2021-07-09 14:52:37 +0100

    Confirm changes

--- src/Command/CheckUrlCommand.php
+++ src/Command/CheckUrlCommand.php
@@ -8,39 +8,35 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Notifier\Message\SmsMessage;
 use Symfony\Component\Notifier\TexterInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Console\Output\ConsoleOutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 
-
-class CreateCheckUrlCommand extends Command
+class CheckUrlCommand extends Command 
 {
-    
     protected static $defaultName = 'app:check-url';
 
-    protected function configure(): void
+    protected function configure()
     {
-        $this->setDescription('Checks if website is till up.');
-
+                
         $this           
+            ->setDescription('Checks if website is till up.')
             ->addArgument('website_url', InputArgument::REQUIRED, 'What is the website url?')
-            ->addArgument('website_title', InputArgument::OPTIONAL, 'What is the website title?')
-            ->addArgument('sender', InputArgument::OPTIONAL, 'Input a sender')
-            ->addArgument('texter', InputArgument::OPTIONAL, 'Input a texter')                        
-        ;
-
+            ->addArgument('website_title', InputArgument::OPTIONAL, 'What is the website title?')  
+            ->setHelp('This command checks if given website is still up');          
+        ;        
     }
- 
+
     public function sendEmail( \Swift_Mailer $mailer)
     {   
         $message = (new \Swift_Message('Website warning message.'))
             ->setFrom('mtekiel777@gmail.com')
             ->setTo('viajador777@egmail.com')
             ->setBody(
-                $this->renderView('Dear Customer, we would like to inform you that your website has problem with response ')                 
-                               
+                $this->renderView('Dear Customer, we would like to inform you that your website has problem with response ')                
+         
             );        
         
         $mailer->send($message);
-
         return;
     }
 
@@ -57,19 +53,18 @@ class CreateCheckUrlCommand extends Command
         return;
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output): int
+    protected function execute(InputInterface $input, OutputInterface $output)
     {
-           
         $websiteUrl = $input->getArgument('website_url');
         $sender = $input->getArgument('sender');
         $texter = $input->getArgument('texter');
 
-        $client = new GuzzleHttp\Client();
+        $client = new \GuzzleHttp\Client();
         $res = $client->request('GET', $websiteUrl);
         
-                
+        // var_dump($res);  
         if($res->getStatusCode() == 200){            
-            $output->write('Status code: 200. We confirm that website is up.  ');
+            $output->write('We confirm that website is up.  ');
             return Command::SUCCESS;
 
         } else {
@@ -79,7 +74,7 @@ class CreateCheckUrlCommand extends Command
             $this->sendEmail($mailer);
             $this->sendSms($texter);                        
             return Command::FAILURE;            
-        }
+       }
     }
-    
 }
+
